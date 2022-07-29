66a97924abf8062f9061585517f8eff9e84c31d9
Carlos H. R. Santos <carlinhos_nino10@hotmail.com>
2022-07-28 22:05:17 -0300

    feat: create categoryService

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,20 @@
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+class CreateCategoryService {
+    constructor(private categoriesRepository: CategoriesRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const categoryAlredyExists = this.categoriesRepository.findByName(name);
+
+        if (categoryAlredyExists) {
+            throw new Error("Caregory Alredy exists!");
+        }
+        this.categoriesRepository.create({ name, description });
+    }
+}
+
+export { CreateCategoryService };
